03f3bf493bddcba8ab73e1b66f96f273331968ef
Nemanja Jocic <nemanja.jocic.dev@gmail.com>
2021-02-05 02:21:13 +0100

    styling header component and providing some example data

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,77 @@
-import React from 'react';
-
-import { Button } from '../Button/Button';
+import React, {useEffect, useState} from 'react';
 import './header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 export interface HeaderProps {
-  user?: {};
-  onLogin: () => void;
-  onLogout: () => void;
-  onCreateAccount: () => void;
+    title: string
+    children: any;
 }
 
-export const Header: React.FC<HeaderProps> = ({ user, onLogin, onLogout, onCreateAccount }) => (
-  <header>
-    <div className="wrapper">
-      <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
-      </div>
-      <div>
-        {user ? (
-          <Button size="small" onClick={onLogout} label="Log out" />
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
-      </div>
-    </div>
-  </header>
-);
+
+export const Header: React.FC<HeaderProps> = ({title, children}) => {
+    const [sideNav, setSideNav] = useState<HTMLElement | null>(null);
+    useEffect(() => {
+        setSideNav(document.getElementById('basic-mobile-side-bar'));
+        return () => {
+            setSideNav(null)
+        }
+    }, [])
+
+    const openMobileMenu = () => {
+        if (sideNav) {
+            sideNav.style.width = "100%"
+        }
+    }
+    const closeMobileMenu = () => {
+        if (sideNav) {
+            sideNav.style.width = "0"
+        }
+    }
+    return (
+        <React.Fragment>
+            <header>
+                <div className="wrapper">
+                    <div>
+                        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <g fill="none" fillRule="evenodd">
+                                <path
+                                    d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+                                    fill="#FFF"
+                                />
+                                <path
+                                    d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+                                    fill="#555AB9"
+                                />
+                                <path
+                                    d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+                                    fill="#91BAF8"
+                                />
+                            </g>
+                        </svg>
+                        <h1>{title}</h1>
+                    </div>
+                    <div className="menu-icon">
+                        <MenuIcon onClick={openMobileMenu}/>
+                    </div>
+
+                    <div className="header-children">
+                        {children}
+                    </div>
+
+                </div>
+            </header>
+            <div id="basic-mobile-side-bar" className='side-nav'>
+                <header>
+                    <div className="wrapper">
+                        <div></div>
+                        <CloseIcon className="close-icon" onClick={closeMobileMenu}> </CloseIcon>
+                    </div>
+                </header>
+                <div className="side-nav-children">
+                    {children}
+                </div>
+            </div>
+        </React.Fragment>
+    )
+};
--- src/pages/Page/Page.stories.tsx
+++ src/pages/Page/Page.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Page, PageProps } from './Page';
+import { Page } from './Page';
 import * as HeaderStories from '../../components/Header/Header.stories';
 
 export default {
@@ -10,14 +10,9 @@ export default {
   component: Page,
 } as Meta;
 
-const Template: Story<PageProps> = (args) => <Page {...args} />;
+const Template: Story = (args) => <Page {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
+export const Primary = Template.bind({});
+Primary.args = {
+  ...HeaderStories.Primary.args,
 };
